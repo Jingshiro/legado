0409ecb29608e5625cbc501d1c9b9e589812b098
Jingshiro <Jingshiro@users.noreply.github.com>
2026-02-01 15:08:29 +0800

    update

--- app/src/main/java/io/legado/app/ui/book/audio/AudioPlayActivity.kt
+++ app/src/main/java/io/legado/app/ui/book/audio/AudioPlayActivity.kt
@@ -1,6 +1,7 @@
 package io.legado.app.ui.book.audio
 
 import android.annotation.SuppressLint
+import android.icu.text.SimpleDateFormat
 import android.os.Build
 import android.os.Bundle
 import android.view.Gravity
@@ -43,7 +44,6 @@ import io.legado.app.utils.sendToClip
 import io.legado.app.utils.showDialogFragment
 import io.legado.app.utils.startActivity
 import io.legado.app.utils.startActivityForBook
-import io.legado.app.utils.toDurationTime
 import io.legado.app.utils.viewbindingdelegate.viewBinding
 import io.legado.app.utils.visible
 import kotlinx.coroutines.Dispatchers.IO
@@ -79,6 +79,13 @@ class AudioPlayActivity :
     private var oldLyric: String? = null
     private var menuCustomBtn: MenuItem? = null
 
+    private val progressTimeFormat by lazy {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            SimpleDateFormat("mm:ss", Locale.getDefault())
+        } else {
+            java.text.SimpleDateFormat("mm:ss", Locale.getDefault())
+        }
+    }
     private val tocActivityResult = registerForActivityResult(TocActivityResult()) {
         it?.let {
             if (it[0] != AudioPlay.book?.durChapterIndex
@@ -177,7 +184,7 @@ class AudioPlayActivity :
         }
         binding.playerProgress.setOnSeekBarChangeListener(object : SeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-                binding.tvDurTime.text = progress.toDurationTime()
+                binding.tvDurTime.text = progressTimeFormat.format(progress.toLong())
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar) {
@@ -370,11 +377,11 @@ class AudioPlayActivity :
         }
         observeEventSticky<Int>(EventBus.AUDIO_SIZE) {
             binding.playerProgress.max = it
-            binding.tvAllTime.text = it.toDurationTime()
+            binding.tvAllTime.text = progressTimeFormat.format(it.toLong())
         }
         observeEventSticky<Int>(EventBus.AUDIO_PROGRESS) {
             if (!adjustProgress) binding.playerProgress.progress = it
-            binding.tvDurTime.text = it.toDurationTime()
+            binding.tvDurTime.text = progressTimeFormat.format(it.toLong())
         }
         observeEventSticky<Int>(EventBus.AUDIO_BUFFER_PROGRESS) {
             binding.playerProgress.secondaryProgress = it
